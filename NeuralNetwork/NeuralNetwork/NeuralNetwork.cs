b0d9be92e5f7ad1e2a6419f348cf8583259a24e4
Andrei Muntean <andrei.muntean.dev@gmail.com>
2016-01-28 17:05:17 +0000

    Started developing the project

---
+++ NeuralNetwork/NeuralNetwork/NeuralNetwork.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NeuralNetwork
+{
+    /// <summary>
+    /// A nonlinear classifier.
+    /// </summary>
+    public class NeuralNetwork<T>
+    {
+        private List<T> knownLabels;
+        
+        /// <summary>
+        /// Initializes a neural network.
+        /// </summary>
+        /// <param name="data">The training data. Each example is represented as a list of features.</param>
+        /// <param name="labels">A list of labels, one for every example.</param>
+        /// <param name="hiddenLayerSize">The number of nodes in each hidden layer.</param>
+        /// <param name="hiddenLayerCount">The number of hidden layers.</param>
+        /// <param name="regularization">The regularization parameter. It is used to avoid overfitting.</param>
+        public NeuralNetwork(IEnumerable<IEnumerable<double>> data, IEnumerable<T> labels, int hiddenLayerSize = 1,
+            int hiddenLayerCount = 1, double regularization = 1.0)
+        {
+            // Stores the unique labels.
+            knownLabels = labels.Distinct().ToList();
+
+            // Trains the network.
+            Train(data, GetIntegerLabel(labels.ToArray()), hiddenLayerSize, hiddenLayerCount, regularization);
+        }
+
+        private IEnumerable<int> GetIntegerLabel(params T[] labels)
+        {
+            return labels.Select(label => knownLabels.IndexOf(label));
+        }
+
+        private IEnumerable<T> GetOriginalLabel(params int[] labels)
+        {
+            return labels.Select(label => knownLabels[label]);
+        }
+
+        private void Train(IEnumerable<IEnumerable<double>> data, IEnumerable<int> labels, int hiddenLayerSize,
+            int hiddenLayerCount, double regularization)
+        {
+
+        }
+
+        /// <summary>
+        /// Predicts a class for the specified data.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <returns></returns>
+        public T Predict(IEnumerable<double> data)
+        {
+            return GetOriginalLabel(0).First();
+        }
+    }
+}
---
+++ NeuralNetwork/NeuralNetwork/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NeuralNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new List<List<double>>
+            {
+                new List<double> { 255, 255, 255 },
+                new List<double> { 255, 0, 0 },
+                new List<double> { 0, 255, 0 },
+                new List<double> { 0, 0, 255 },
+                new List<double> { 255, 255, 0 },
+                new List<double> { 255, 0, 255 },
+                new List<double> { 0, 255, 255 },
+                new List<double> { 120, 120, 120 },
+                new List<double> { 0, 0, 0 }
+            };
+
+            string[] labels =
+            {
+                "White",
+                "Red",
+                "Green",
+                "Blue",
+                "Yellow",
+                "Purple",
+                "Cyan",
+                "Gray",
+                "Black"
+            };
+
+            var tests = new List<List<double>>
+            {
+                new List<double> { 235, 15, 92 },
+                new List<double> { 35, 64, 249 },
+                new List<double> { 25, 15, 48 },
+                new List<double> { 123, 100, 130 }
+            };
+
+            var neuralNetwork = new NeuralNetwork<string>(data, labels);
+
+            foreach (var test in tests)
+            {
+                Console.WriteLine(string.Join(" ", test) + " is " + neuralNetwork.Predict(test));
+            }
+
+            Console.Read();
+        }
+    }
+}
